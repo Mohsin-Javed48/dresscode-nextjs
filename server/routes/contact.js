0cd99619d89467058818f854d6eb72ad1571ddb2
Mohsin Javed <mohsinjavedpc@gmail.com>
2025-10-08 20:05:27 +0500

    server delete

--- server/routes/contact.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { handleCreateContact } = require("../controllers/contact");
-const router = express.Router();
-
-router.post("/", handleCreateContact);
-
-module.exports = router;
